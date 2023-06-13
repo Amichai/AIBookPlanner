4dd7cf5d0122fd94402f7335ee2318f979768434
amichailevy <amichailevy@Amichais-MacBook-Pro.local>
2023-06-13 11:09:24 -0400

    Starter Vue3 project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <div class="wrapper flow">
+      <h1>Title</h1>
+      <p>Who moved my cheese dolcelatte port-salut. Bavarian bergkase edam cream cheese caerphilly danish fontina
+        everyone loves feta squirty cheese. Goat monterey jack cheese and biscuits st. agur blue cheese goat cut the
+        cheese croque monsieur cheesy grin. Hard cheese cow squirty cheese feta blue castello cauliflower cheese queso
+        edam. Lancashire.</p>
+      <p>The big cheese swiss parmesan. Ricotta cottage cheese feta cheese triangles melted cheese cheesy feet rubber
+        cheese swiss. Cheese and biscuits cheese slices smelly cheese cheese and wine edam fondue pepper jack say
+        cheese. Boursin cheese triangles cheese and biscuits bocconcini cow cottage cheese croque monsieur port-salut.
+        Babybel red leicester roquefort parmesan.</p>
+      <p>Cheese strings chalk and cheese boursin. Airedale cheeseburger cheesecake roquefort dolcelatte chalk and cheese
+        caerphilly cheese triangles. Boursin feta babybel cheesecake rubber cheese cauliflower cheese cow red leicester.
+        Cheese strings monterey jack cream cheese smelly cheese stilton feta melted cheese cow. Red leicester rubber
+        cheese fondue say cheese.</p>
+      <p>Cheesy grin cauliflower cheese danish fontina. Caerphilly taleggio gouda squirty cheese smelly cheese
+        mascarpone cheesy grin pepper jack. Bavarian bergkase red leicester camembert de normandie camembert de
+        normandie jarlsberg boursin jarlsberg goat. Pecorino.</p>
+      <p>Ricotta mozzarella dolcelatte. Goat rubber cheese queso edam cheese triangles fromage frais cheddar danish
+        fontina. Hard cheese cauliflower cheese stilton swiss camembert de normandie fondue everyone loves cheese
+        strings. Brie gouda jarlsberg paneer lancashire cottage cheese fromage frais.</p>
+      <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, voluptatum.</h3>
+      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, voluptatum.</h2>
+      <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, voluptatum.</h3>
+    </div>
+  </main>
+</template>
