ff223fe3dd7d55f3f46ca9383523fad21f639b16
amichailevy <amichailevy@Amichais-MacBook-Pro.local>
2023-06-14 22:32:24 -0400

    broken state

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,30 +3,8 @@ import { ref, computed } from 'vue'
 import { getTitles, getEpigraphs } from '../apiHelper.js'
 import LoadingDialog from '../components/LoadingDialog.vue';
 
-
 const description = ref('')
 const isAIThinking = ref(false)
-
-const placeholderOptions = [
-  'A small, furry rabbit embarks on an unlikely adventure to save a stranded whale.',
-  'Following an unexplainable beacon, a gang of rebellious teens take on a powerful evil tyrant.',
-  'A mischievous rock wanders the world, collecting insights from its travels and sharing them with anyone willing to listen.',
-  'When a talking raccoon is mistaken for a bandit, the unlikeliest of friends must join forces to save the day.',
-  'A medieval knight teams up with a crew of modern-day urbanites to take down an alien invasion.',
-  'Three witches must cook up a magical potion to reverse a curse before their town is destroyed.',
-  'A single forgotten happy hour leads to one epic night of adventure and excitement.',
-  'A reluctant space explorer embarks on a mission to locate a mysterious planet that could hold the key to saving humanity.',
-  'A detective with extraordinary powers and an unlikely sidekick team up to unravel a centuries-old mystery.',
-  'When a toy box transports two children to a distant galaxy, they must enlist the help of intergalactic superheroes to make their way back home.',
-  'Discover how you can save money by eating cereal for every meal',
-  'The bizarre history of the bathroom fixture from ancient Egypt to modern day use',
-  'Plant communication',
-  'Uncovering the fascinating secrets of bees',
-  "The intricate formalities of visiting a foreign royal family, so you don't make any faux pas!"
-]
-
-const placeholder = placeholderOptions[Math.floor(Math.random() * placeholderOptions.length)];
-
 
 const titleOptions = ref([])
 const epigraphOptions = ref([])
@@ -144,10 +122,11 @@ const generateBook = () => {
       <h3>
         Step 1 - What is this book about?
       </h3>
-      <textarea class="text-area" rows="3" :placeholder="placeholder" v-model="description"></textarea>
+      <textarea class="text-area" rows="3" placeholder="Describe the book want to generate"
+        v-model="description"></textarea>
 
       <h3>
-        Step 2 - Select a title:
+        Step 2 - Select a title
       </h3>
       <button class="button" @click="generateTitles" :disabled="!description">
         Show Me Some Titles
@@ -166,11 +145,20 @@ const generateBook = () => {
         </li>
       </ul>
       <h3>Step 3 - Select an epigraph</h3>
-      <button class="button" @click="generateEpigraphs" :disabled="!canGenerateBook">
-        Show Me (fake) Epigraphs
-      </button>
 
-      <p class="subtitle" v-if="epigraphOptions.length"><i>Warning! All these quotes were invented by AI and are likely not
+      <VTooltip>
+        <div>
+          <button class="button" @click="generateEpigraphs" :disabled="!canGenerateBook">
+            Show Me (fake) Epigraphs
+          </button>
+        </div>
+        <template #popper v-if="!canGenerateBook">
+          Click a book title to select it
+        </template>
+      </VTooltip>
+
+      <p class="subtitle" v-if="epigraphOptions.length"><i>Warning! All these quotes were invented by AI and are likely
+          not
           real</i></p>
 
       <ul class="title-options" v-show="description">
@@ -182,12 +170,19 @@ const generateBook = () => {
       </ul>
 
       <h3>
-        Step 4 - Make a Book!
+        Step 4 - Generate Book
       </h3>
-      <button class="button" @click="generateBook" :disabled="!canGenerateBook">
-        <!-- <img src="/src/assets/book_emoji.png" alt="book_emoji" class="fire-emoji"> -->
-        Let's do this!
-      </button>
+      <VTooltip>
+        <div>
+          <button class="button" @click="generateBook" :disabled="!canGenerateBook">
+            <!-- <img src="/src/assets/book_emoji.png" alt="book_emoji" class="fire-emoji"> -->
+            Let's do this!
+          </button>
+        </div>
+        <template #popper v-if="!canGenerateBook">
+          Click a book title to select it
+        </template>
+      </VTooltip>
     </div>
     <br><br><br>
   </main>
